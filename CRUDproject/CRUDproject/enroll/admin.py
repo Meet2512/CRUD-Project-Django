69e90a94c35a1d4e7cee3ffb05a901a80929f20f
Meet2512 <83955900+Meet2512@users.noreply.github.com>
2021-09-26 14:05:58 +0530

    Added the app
    Application

---
+++ CRUDproject/CRUDproject/enroll/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import student
+
+# Register your models here.
+@admin.register(student)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'password')
